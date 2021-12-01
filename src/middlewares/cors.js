a9525e31696367668390807df0df296b5f3851cb
莫帆 <wanghaigang@innotechx.com>
2021-12-01 20:36:15 +0800

    fix: 设置精准的 cors 响应头，支持 Credentials include

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,7 +1,9 @@
 export default function(req, res, next) {
-  res.append('access-control-allow-origin', "*")
-  res.append('access-control-allow-credentials', true)
-  res.append('access-control-allow-headers', "*")
+  const { origin } = new URL(req.headers.referer)
+  res.append('Access-Control-Allow-Origin', origin)
+  res.append('Access-Control-Allow-Credentials', true)
+  res.append('Access-Control-Allow-Methods', "*")
+  res.append('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, X-HTTP-Method-Override, Cookie, X-Token, X-Device, X-Plt, X-Ver, X-Page-Id, X-Chl, X-Feature, X-Tag, X-Tk, X-Epid, X-App, X-Os, X-Mock-Domain")
 
   next();
 }
