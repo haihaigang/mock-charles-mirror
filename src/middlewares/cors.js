c1893b8d44b0e4684a8cf9ee9bed304f6d306030
莫帆 <wanghaigang@innotechx.com>
2021-12-02 16:35:20 +0800

    fix: 修复没有 referer 时正常设置 cors

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,5 +1,9 @@
 export default function(req, res, next) {
-  const { origin } = new URL(req.headers.referer)
+  let origin = '*'
+  if (req.headers.referer) {
+    const refUrl = new URL(req.headers.referer)
+    origin = refUrl.origin
+  }
   res.append('Access-Control-Allow-Origin', origin)
   res.append('Access-Control-Allow-Credentials', true)
   res.append('Access-Control-Allow-Methods', "*")
