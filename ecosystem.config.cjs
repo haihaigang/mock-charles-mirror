b3fd6f2e929461b8c509617aecd13746f6ac94bc
莫帆 <wanghaigang@innotechx.com>
2021-12-02 14:43:30 +0800

    chore: 增加线上的配置

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,30 +1,17 @@
 module.exports = {
   apps : [{
-    name: 'API',
-    script: 'app.js',
-
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    name: 'Mock Charles Mirror',
+    cwd: './src',
+    script: './index.js',
     args: 'one two',
+    node_args: '--harmony',
     instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
     env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
+      NODE_ENV: 'production',
+      PORT: 8110
     }
-  }],
-
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
+  }]
 };
