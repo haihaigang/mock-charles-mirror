1e8812198f0c05fce09c352dc1ce174295a29c71
莫帆 <wanghaigang@innotechx.com>
2022-07-08 16:40:57 +0800

    feat: 添加功能菜单和新增域名功能

--- src/routers/domain.js
+++ src/routers/domain.js
@@ -21,6 +21,18 @@ router.post('/changeMockDomain', (req, res, next) => {
   res.json({})
 })
 
+
+router.post('/addMockDomain', (req, res, next) => {
+  let { domain } = req.body
+  if (!domain) throw new Error('参数错误，缺少参数 domain')
+
+  let domains = getMockDomains()
+  if (domains.includes(domain)) throw new Error('参数错误，domain 已经存在')
+
+  fs.mkdirSync(path.resolve(process.cwd(), '../mock', domain))
+  res.json({})
+})
+
 function getMockDomains() {
   let files = fs.readdirSync(path.resolve(process.cwd(), '../mock'))
   if (files) {
