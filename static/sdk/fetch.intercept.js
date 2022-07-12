1c1a1a94f0989186cbc42ee5d0d53470cfd1f44f
“wanghaigang” <haihaigang@163.com>
2022-07-12 23:58:20 +0800

    feat: 添加 fetch js sdk，更新接入文档

---
+++ static/sdk/fetch.intercept.js
@@ -0,0 +1,21 @@
+(function(window, location){
+  let _fetch = window.fetch
+  window.fetch = function (url, params) {
+    if (isFromMock(url, params)) {
+      let domains = /^https?:\/\/(.+?)\/.*/.exec(url)
+      if (domains) {
+        let domain = domains[1]
+        url = url.replace(domain, `${location.protocol}//${location.host}` | 'http://127.0.0.1:3007')
+        params.headers['x-mock-domain'] = domain
+        console.log(['__MOCK__', url])
+      }
+    }
+    
+    return _fetch(url, params)
+  }
+  
+  let reg_mock = /__mock__/i
+  function isFromMock(url, params) {
+    return reg_mock.test(url) || (params && reg_mock.test(params.body)) || reg_mock.test(location.search)
+  }
+})(window, location)
