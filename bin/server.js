14590e5c50667eac53b65ca15641c895e7094290
莫帆 <wanghaigang@innotechx.com>
2022-07-15 14:24:46 +0800

    feat: 启动命令增加参数

--- bin/server.js
+++ bin/server.js
@@ -1,8 +1,24 @@
 #!/usr/bin/env node
 
 import { spawn } from "child_process"
+import minimist from "minimist"
 
-process.title = 'mock-charles-mirror'
+let argv = minimist(process.argv.slice(2))
+
+if (argv.h || argv.help) {
+  console.log([
+    'usage: mock-charles-mirror [options]',
+    '',
+    'options:',
+    '  -p --port       使用的端口号。[3007]',
+    '  -h --help       查看所有命令。',
+  ].join('\n'))
+  process.exit()
+}
+
+if (argv.p || argv.port) {
+  process.env.PORT = argv.p || argv.port
+}
 
 let url = new URL('../src/index.js', import.meta.url)
 const result = spawn(
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { env } from 'process'
 import initMiddlewares from './middlewares/index.js'
 import initRouter from './routers/index.js'
 
-const { name, PORT = 3007 } = env
+const { name = 'mock-charles-mirror', PORT = 3007 } = env
 
 function start() {
   const app = new Express()
