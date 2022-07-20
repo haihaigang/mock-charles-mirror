cffdf59aa72e5e3a4c728e747e8f9a8c327fa679
“wanghaigang” <haihaigang@163.com>
2022-07-20 14:06:55 +0800

    fix: 调整跨域请求头为 ”*“

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -7,7 +7,7 @@ export default function(req, res, next) {
   res.append('Access-Control-Allow-Origin', origin)
   res.append('Access-Control-Allow-Credentials', true)
   res.append('Access-Control-Allow-Methods', "*")
-  res.append('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, X-HTTP-Method-Override, Cookie, X-Token, X-Device, X-Plt, X-Ver, X-Page-Id, X-Chl, X-Feature, X-Tag, X-Tk, X-Epid, X-App, X-Os, X-Mock-Domain")
+  res.append('Access-Control-Allow-Headers', "*")
 
   next();
 }
